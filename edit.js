f95f9a21e4c02ffefd8bba3decb4509f24d4526e
NobleWolf42 <greenboyb1@gmail.com>
2019-08-12 16:28:07 -0400

    added JSON.stringify to send function

--- edit.js
+++ edit.js
@@ -111,7 +111,7 @@ function sendjsondata() {
     dataObj.ski.notes = document.getElementById("skinote").value;
 
     jsonhttp.open("POST", jsonurl, true);
-    jsonhttp.send(dataObj);
+    jsonhttp.send(JSON.stringify(dataObj);
 };
 
 window.onload=update;
