a6e36e8934db3d736262769d8c4f8ba3fae8f2fb
NobleWolf42 <greenboyb1@gmail.com>
2019-08-14 11:27:53 -0400

    Def Working

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ function update() {
         var key;
         for (key in obj1) {
             document.getElementById(key + "stat").innerHTML = opcl[obj1[key].status];
-            document.getElementById(key + "note").innerHTML = obj1.key.notes;
-            document.getElementById(key + "pic").src = obj1.key.status + '.png';
+            document.getElementById(key + "note").innerHTML = obj1[key].notes;
+            document.getElementById(key + "pic").src = obj1[key].status + '.png';
         }
     }
 };
