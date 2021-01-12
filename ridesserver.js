c4ede076fcf1999aa8eb0b42166841b9f1be1a9d
NobleWolf42 <greenboyb1@gmail.com>
2021-01-12 01:46:25 -0500

    fixed new server issue

--- ridesserver.js
+++ ridesserver.js
@@ -20,7 +20,7 @@ app.get("/getjson", function (request, response) {
 
 app.post("/submitjson", function(request, response) {
     jsondata = JSON.stringify(request.body);
-    fs.writeFile("/var/www/html/bencarpenterit.com/public/projects/RideStatus/data.json", JSON.stringify(request.body), function(err) {
+    fs.writeFile("/var/www/html/bencarpenterit.com/public_html/projects/RideStatus/data.json", JSON.stringify(request.body), function(err) {
         if(err) {
             console.log("Save File Failed.");
             console.log(err);
